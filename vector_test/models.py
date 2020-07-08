6430d620c96930f168b5859b42b49a753d501b23
Ivan A Kireev <ivkireev@yandex.ru>
2020-07-08 12:28:38 +0300

    model dynamic load

--- vector_test/models.py
+++ vector_test/models.py
@@ -1,9 +1,7 @@
-from sklearn.linear_model import LogisticRegression
-from lightgbm import LGBMClassifier
-
-
 def create(type, params):
-    cls = globals().get(type, None)
+    i = type.split('.')
+    mod = __import__('.'.join(i[:-1]), fromlist=[i[-1]])
+    cls = getattr(mod, i[-1])
     if cls is None:
         raise AttributeError(f'Unknown model type: "{type}"')
 
