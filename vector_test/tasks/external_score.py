d9d4842132b8690056beaa08cdf145f3099fddf3
Ivan A Kireev <ivkireev@yandex.ru>
2020-07-16 10:25:27 +0300

    error handling for external score task

--- vector_test/tasks/external_score.py
+++ vector_test/tasks/external_score.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 
 import luigi
@@ -18,10 +19,21 @@ class ExternalScore(luigi.Task):
 
     def run(self):
         conf = Config.read_file(self.conf)
+        on_error = conf.error_handling
 
-        path = os.path.join(conf.root_path, self.external_path)
-        with open(path, 'r') as f:
-            external_scores = json.load(f)
+        try:
+            path = os.path.join(conf.root_path, self.external_path)
+            with open(path, 'r') as f:
+                external_scores = json.load(f)
+        except BaseException:
+            if on_error == conf.ON_ERROR_SKIP:
+                external_scores = None
+                logging.getLogger('luigi-interface').exception('Fail', stack_info=True)
+            elif on_error == conf.ON_ERROR_FAIL:
+                raise
+            else:
+                raise AssertionError(f'Unknown error_handling: "{on_error}"')
 
         with self.output().open('w') as f:
+            external_scores = [] if external_scores is None else external_scores
             json.dump(external_scores, f, indent=2)
